cb3dc6fd1a996c501ddc7e566edb26a906ecd9f2
naxer-12 <rexan.2212@gmail.com>
2022-09-29 15:54:25 -0400

    Added react with redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state/index";
 function App() {
+  const account = useSelector((state) => state.account);
+  const dispatch = useDispatch();
+
+  const { depositMoney, withdrawMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+  console.log(depositMoney,withdrawMoney);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{account}</h1>
+      <button onClick={() => depositMoney(1000)}>DEPOSIT</button>
+      <button onClick={() => withdrawMoney(10)}>WITHDRAW</button>
     </div>
   );
 }
--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,3 +1,10 @@
 import {combineReducers} from "redux";
 
 import accountReducer from "./account-reducers";
+
+
+const reducers = combineReducers({
+    account : accountReducer
+});
+
+export default reducers;
